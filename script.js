f908b92743865b4095eca891774d6f4f4cac5f6d
Sienna Szymanski <szymanski.sienna@gmail.com>
2026-09-14 20:54:24 -0600

    Starting

---
+++ script.js
@@ -0,0 +1,17 @@
+function toggleMenu() {
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+}
+document.addEventListener("DOMContentLoaded", function() {
+    var typed = new Typed("#typing-effect", {
+        strings: ["Journalist.", "Photographer.", "Legislative Researcher."],
+        typeSpeed: 100,      // How fast it types
+        backSpeed: 50,       // How fast it deletes
+        backDelay: 2000,     // Wait 2 seconds before deleting
+        loop: true,          // Keep looping forever
+        showCursor: true,    // Show the blinking cursor
+        cursorChar: '|',     // The cursor character
+    });
+});
